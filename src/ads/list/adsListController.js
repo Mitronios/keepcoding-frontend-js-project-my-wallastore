c0fe4471a88d54c8de2b9999a0cdf17ff91f7100
Miguel Angel Gonzales Melgar <mitronios@gmail.com>
2025-04-26 18:23:52 -0400

    style: create loader and error message, minor styles adjustment to product view.

--- src/ads/list/adsListController.js
+++ src/ads/list/adsListController.js
@@ -1,13 +1,19 @@
 import { getAdsList } from "./adsListModel.js";
-import { showAdsList } from "./adsListView.js";
+import {
+	renderAdsError,
+	renderAdsLoading,
+	showAdsList,
+} from "./adsListView.js";
 
 export async function adsListController(container) {
+	renderAdsLoading(container);
+
 	try {
 		// show ads
 		const ads = await getAdsList();
 		console.log(ads);
 		showAdsList(container, ads);
 	} catch (error) {
-		console.error(error);
+		renderAdsError(container, error.message);
 	}
 }
--- src/ads/list/adsListView.js
+++ src/ads/list/adsListView.js
@@ -11,17 +11,17 @@ export function showAdsList(container, ads) {
 	const renderAds = ads
 		.map((ad) => {
 			return `
-            <div class="border border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div class="border border-gray-400 bg-white rounded-b p-4 flex flex-col justify-between leading-normal">
             <div class="p-2">
             <p class="text-sm text-gray-600 flex items-center">
             <img src="${ad.image || "placeholder.jpg"}" alt="${
 				ad.name
 			} class="h-48 w-96 object-cover mb-4">
             </p>
-            <h2 class="text-gray-900 font-bold text-xl mb-2">${ad.name}</h2>
+            <h2 class="text-gray-900 font-bold text-xl my-2">${ad.name}</h2>
             <p class="text-gray-700 text-base">${ad.description}</p>
             <p class="font-bold mt-2">${ad.price} USD</p>
-            <p class="italic text-sm">${
+            <p class="italic mt-2 text-red-400 text-sm">${
 							ad.sale ? ad.sale : "Looking for purchase"
 						}</p>
             <a href="#/ads/${ad.id}"
@@ -36,4 +36,16 @@ export function showAdsList(container, ads) {
                 ${renderAds}
             </div>
         `;
+}
+
+export function renderAdsError(container, message) {
+	container.innerHTML = `<p class="text-red-500 text-center mt-10">${message}</p>`;
+}
+
+export function renderAdsLoading(container) {
+	container.innerHTML = `<button type="button" class="flex bg-indigo-500 p-4" disabled>
+  <span><svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><circle cx="12" cy="2.5" r="1.5" opacity=".14"/><circle cx="16.75" cy="3.77" r="1.5" opacity=".29"/><circle cx="20.23" cy="7.25" r="1.5" opacity=".43"/><circle cx="21.50" cy="12.00" r="1.5" opacity=".57"/><circle cx="20.23" cy="16.75" r="1.5" opacity=".71"/><circle cx="16.75" cy="20.23" r="1.5" opacity=".86"/><circle cx="12" cy="21.5" r="1.5"/><animateTransform attributeName="transform" type="rotate" calcMode="discrete" dur="0.75s" values="0 12 12;30 12 12;60 12 12;90 12 12;120 12 12;150 12 12;180 12 12;210 12 12;240 12 12;270 12 12;300 12 12;330 12 12;360 12 12" repeatCount="indefinite"/></g></svg></span>
+  <span class="text-white ml-3">Loading products…</span>
+</button>
+`;
 }
