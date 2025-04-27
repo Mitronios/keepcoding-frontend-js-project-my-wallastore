2544397e3f219711177192fa9dca42f0f2143fb6
Miguel Angel Gonzales Melgar <mitronios@gmail.com>
2025-04-27 17:44:04 -0400

    cleanup: remove unused imports.

--- src/ads/detail/adDetailModel.js
+++ src/ads/detail/adDetailModel.js
@@ -1,5 +1,4 @@
 import { getById } from "../../core/apiClient.js";
-import { API_ENDPOINT_ADS } from "../../core/apiClient.js";
 
 export const getAdDetails = async (id) => {
 	return await getById(`/${id}`);
